5e1a34c98588da06f76d3b51aa6a33d4c55a9b36
Walter Emilio Delgado Yucra <110571114+WedyDev@users.noreply.github.com>
2025-10-26 17:11:59 -0500

    Update environment.prod.ts

--- src/app/environmets/environment.prod.ts
+++ src/app/environmets/environment.prod.ts
@@ -4,11 +4,11 @@ export const environment = {
   production: false,
 
   // API endpoint for your backend
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://vettracebackend-1.onrender.com',
   //apiUrl: 'http://102.37.216.79:80',
   //apiUrl:'http://localhost:8080',
   // WebSocket URL (if you use sockets)
-  socketUrl: 'ws://localhost:8080/ws',
+  socketUrl: 'ws:https://vettracebackend-1.onrender.com/ws',
 
   // Feature flags
   featureFlags: {
